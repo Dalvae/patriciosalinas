5aacb126235329c651cccf0bdd97fadd779eda81
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-08-12 15:25:07 -0400

    hybrid generation for dynamic route leng

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
+  output: "hybrid",
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en", "sv"],
