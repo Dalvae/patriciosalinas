4c25c6fe44cab7092ca9fe8964b22667b5646f0d
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-08-30 01:29:34 -0400

    Margin con content

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -35,7 +35,7 @@ const HomePage: React.FC<HomePageProps> = ({ content, projects }) => {
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
       {/* Content Column */}
-      <div className="w-full md:w-1/3 p-6">
+      <div className="w-full md:w-1/3 p-12 mt-9">
         <div dangerouslySetInnerHTML={{ __html: content }} />
       </div>
 
