aeee20db288eb55dda1f22581a24044423f93838
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-09-04 16:32:03 -0400

    Minor styles

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -30,7 +30,7 @@ const HomePage: React.FC<HomePageProps> = ({ content, projects, lang }) => {
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
       {/* Content Column */}
-      <div className="w-full md:w-1/3 p-12 mt-9 text-justify">
+      <div className="w-full md:w-1/3 lg:p-12 p-3 mt-9 text-justify">
         <div dangerouslySetInnerHTML={{ __html: content }} />
       </div>
       {/* Project Gallery */}
